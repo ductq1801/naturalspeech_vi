d09216eac9c955eb69fbbc35384c8989ef487795
ductq1801 <ductq1801@gmail.com>
2023-08-30 21:12:41 +0700

    new symbols

--- text/symbols.py
+++ text/symbols.py
@@ -6,7 +6,7 @@ Defines the set of symbols used in text input to the model.
 _pad = "_"
 _punctuation = ';:,.!?¡¿—…"«»“” '
 #_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼ¹²³⁴⁵⁶⁷⁸ᴬᴮᶜᴰˀʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼ¹²³⁴⁵⁶⁷⁸ᴬᴮᶜᴰᵐᵖˀʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 _letters = 'AÁÀẠẢÃĂẰẮẶẲẴÂẤẦẬẨẪBCDĐEÉÈẺẼẸÊẾỀỂỄỆFGHIÍÌỊỈĨJKLMNOÓÒỌỎÕƠỚỜỢỠỞÔỐỒỘỖỔPQRSTUÚÙỤỦŨƯỨỪỰỮỬVWXYZaáàạảãăắằặẳẵâấầậẩẫbcdđeéèẻẽẹêếềểễệfghiíìịỉĩjklmnoóòọỏõơớờợỡởôốồộỗổpqrstuúùụủũưứừựữửvwxyz'
 
 # Export all symbols:
@@ -14,4 +14,4 @@ symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
-#print('ɲ' in symbols)
+#print('ᵖ' in symbols)
